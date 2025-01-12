73c4c63ce848462782fd7e7aab492020422191d2
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-01-12 18:51:48 +0300

    feat: loading and error situations

--- src/pages/Countries/Countries.tsx
+++ src/pages/Countries/Countries.tsx
@@ -4,16 +4,28 @@ import { fetchCountries } from 'features/countries/countriesSlice';
 import { CountriesList } from 'components/CountriesList';
 
 export const Countries = () => {
-    const dispatch = useAppDispatch();
-    const countries = useAppSelector(state => state.countries.list);
+  const dispatch = useAppDispatch();
+  const countries = useAppSelector(state => state.countries.list);
+  const error = useAppSelector(state => state.countries.error);
+  const status = useAppSelector(state => state.countries.status);
 
-    useEffect(() => {
-      if (!countries.length) {
-        dispatch(fetchCountries());
-      }
-    }, [countries.length, dispatch]);
+  useEffect(() => {
+    if (!countries.length) {
+      dispatch(fetchCountries());
+    }
+  }, [countries.length, dispatch]);
+
+  if (error) {
+    return <h1>{error}</h1>
+  }
+
+  if (status === 'loading') {
+    return <h1>Loading...</h1>
+  }
 
   return (
-    <CountriesList countries={countries} />
+    <>
+      {countries.length > 0 && status === 'succeeded' && <CountriesList countries={countries} />}
+    </>
   )
 }
