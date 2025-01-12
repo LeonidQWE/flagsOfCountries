a1d78d3a8d8a63cdc9f6119f3da745e7c735e90f
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-01-12 18:39:54 +0300

    feat: CountryCard component

--- src/components/CountriesList/CountriesList.tsx
+++ src/components/CountriesList/CountriesList.tsx
@@ -1,5 +1,6 @@
 import { ShortCountryInfo } from 'types';
 import styles from './CountriesList.module.scss';
+import { CountryCard } from 'components/CountryCard';
 
 interface CountriesListProps {
   countries: ShortCountryInfo[];
@@ -9,7 +10,7 @@ export const CountriesList = ({ countries }: CountriesListProps) => {
   return (
     <div className={styles.countriesList}>
       {countries.map((country, index) => (
-        <div></div>
+        <CountryCard key={index} country={country} />
       ))}
     </div>
   )
