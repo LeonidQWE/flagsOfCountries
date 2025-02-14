4cb4c7e56e2b6ed8d444cd5b6153d422c004c43c
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-02-14 12:32:01 +0300

    fix styles

--- src/components/BackLink/BackLink.tsx
+++ src/components/BackLink/BackLink.tsx
@@ -1,6 +1,6 @@
+import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import styles from './BackLink.module.scss';
-import { useNavigate } from 'react-router-dom';
 
 export const BackLink = () => {
   const navigate = useNavigate();
