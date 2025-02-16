4065d6af6eacc20f70d3c7c5f4d677d7a6be18a3
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-02-16 12:37:16 +0300

    add parametr in slice

--- src/components/SearchEngine/SearchEngine.tsx
+++ src/components/SearchEngine/SearchEngine.tsx
@@ -1,9 +1,23 @@
+import { ChangeEvent } from 'react';
+import { useAppDispatch, useAppSelector } from 'store';
+import { selectSearch, setSearch } from 'features/controls/controlsSlice';
 import styles from './SearchEngine.module.scss';
 
 export const SearchEngine = () => {
+  const dispatch = useAppDispatch();
+  const searchValue = useAppSelector(selectSearch)
+
+  const handleChangeSearchCountry = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearch(event.target.value));
+  }
+
   return (
     <div className={styles.container}>
-      <input type="text" className={styles.input}/>
+      <input
+        type="text"
+        className={styles.input}
+        value={searchValue}
+        onChange={handleChangeSearchCountry}/>
     </div>
   )
 }
--- src/features/controls/controlsSlice.ts
+++ src/features/controls/controlsSlice.ts
@@ -3,10 +3,12 @@ import { Regions } from 'types';
 
 type ControlsSlice = {
   region: Regions | '';
+  search: string;
 }
 
 const initialState: ControlsSlice = {
   region: '',
+  search: '',
 };
 
 const controlsSlice = createSlice({
@@ -16,13 +18,17 @@ const controlsSlice = createSlice({
     setRegion: (state, action: PayloadAction<Regions>) => {
       state.region = action.payload;
     },
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload
+    },
     clearRegion: () => initialState,
   },
   selectors: {
     selectRegion: (state) => state.region,
+    selectSearch: (state) => state.search
   }
 })
 
 export const controlsReducer = controlsSlice.reducer;
-export const { setRegion, clearRegion } = controlsSlice.actions;
-export const { selectRegion } = controlsSlice.selectors;
+export const { setRegion, setSearch, clearRegion } = controlsSlice.actions;
+export const { selectRegion, selectSearch } = controlsSlice.selectors;
